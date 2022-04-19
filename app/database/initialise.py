412d264455a72f439e685c46096d434daf8d32c7
Jacob Gluszek <jacobgluszek03@gmail.com>
2022-04-19 16:38:50 +0200

    consistent strings

--- app/database/initialise.py
+++ app/database/initialise.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import Session
 from app.database import Base, engine
 
 
-logger = logging.getLogger('main')
+logger = logging.getLogger("main")
 
 def initialise(db: Session) -> None:
     Base.metadata.create_all(bind=engine)
-    logger.info('Database Tables created.')
+    logger.info("Database Tables created.")
--- app/database/session.py
+++ app/database/session.py
@@ -5,17 +5,17 @@ from sqlalchemy.orm import sessionmaker
 from app.core import config
 
 
-logger = logging.getLogger('main')
-database_name = 'postgres'
+logger = logging.getLogger("main")
+database_name = "postgres"
 
 if database_name in config.db.DATABASE_URI:
     engine = create_engine(
         config.db.DATABASE_URI, connect_args={"check_same_thread": False}
     )
-    logger.info(f'Connected to {database_name} database.')
+    logger.info(f"Connected to {database_name} database.")
 else:
     engine = create_engine(config.db.DATABASE_URI, pool_pre_ping=True)
-    logger.info('Created a SQLite database.')
-    logger.info('Connected to the SQLite database.')
+    logger.info("Created a SQLite database.")
+    logger.info("Connected to the SQLite database.")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
