5c412bdc7412404c93e73187049266759a58cff7
Jacob Gluszek <jaqobek1995@gmail.com>
2022-04-22 16:08:25 +0200

    fixed engine initialization

--- app/database/session.py
+++ app/database/session.py
@@ -9,12 +9,12 @@ logger = logging.getLogger("main")
 database_name = "postgres"
 
 if database_name in config.db.DATABASE_URI:
+    engine = create_engine(config.db.DATABASE_URI, pool_pre_ping=True)
+    logger.info(f"Connected to {database_name} database.")
+else:
     engine = create_engine(
         config.db.DATABASE_URI, connect_args={"check_same_thread": False}
     )
-    logger.info(f"Connected to {database_name} database.")
-else:
-    engine = create_engine(config.db.DATABASE_URI, pool_pre_ping=True)
     logger.info("Created a SQLite database.")
     logger.info("Connected to the SQLite database.")
 
